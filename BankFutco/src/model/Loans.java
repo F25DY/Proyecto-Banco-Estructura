2eebaaf55504c47395af6cca00b538e30f1bfa83
JhoynerZabalsa06 <sabalzapuertaj@gmail.com>
2025-11-07 13:22:56 -0500

    loans service(clase, implemetacion de interaz) jhoyner zabalsa

--- BankFutco/src/model/Loans.java
+++ BankFutco/src/model/Loans.java
@@ -5,71 +5,82 @@ import java.time.LocalDate;
 
 public class Loans{
 
-	// Atributos
-	private String loanId;
-	private LocalDate date;
-	private String type; // e.g. "Home", "Vehicle", "Personal"
-	private BigDecimal totalLoan;
-	private BigDecimal amountPaid;
-	private BigDecimal outstandingAmt;
+    // Atributos
+    private String loanId; // Clave para la búsqueda
+    private LocalDate date;
+    private String type; // e.g. "Home", "Vehicle", "Personal"
+    private BigDecimal totalLoan;
+    private BigDecimal amountPaid;
+    private BigDecimal outstandingAmt;
 
-	// Constructor
-	public Loans (){
+    // Constructor sin argumentos
+    public Loans (){
+    }
 
-	}
+    // CONSTRUCTOR COMPLETO (¡CORREGIDO para incluir loanId!)
+    public Loans(String loanId, LocalDate date, String type, BigDecimal totalLoan, BigDecimal amountPaid, BigDecimal outstandingAmt) {
+        this.loanId = loanId;
+        this.date = date;
+        this.type = type;
+        this.totalLoan = totalLoan;
+        this.amountPaid = amountPaid;
+        this.outstandingAmt = outstandingAmt;
+    }
 
-	public Loans(LocalDate date, String type, BigDecimal totalLoan, BigDecimal amountPaid, BigDecimal outstandingAmt) {
-		this.date = date;
-		this.type = type;
-		this.totalLoan = totalLoan;
-		this.amountPaid = amountPaid;
-		this.outstandingAmt = outstandingAmt;
-	}
 
+    // Getters y Setters
+    
+    public String getLoanId() {
+        return loanId;
+    }
 
-	// Getters y Setters
-	public LocalDate getDate() {
-		return date;
-	}
-	
-	public void setDate(LocalDate date) {
-		this.date = date;
-	}
-	public String getType() {
-		return type;
-	}
-	public void setType(String type) {
-		this.type = type;
-	}
-	public BigDecimal getTotalLoan() {
-		return totalLoan;
-	}
-	public void setTotalLoan(BigDecimal totalLoan) {
-		this.totalLoan = totalLoan;
-	}
-	public BigDecimal getAmountPaid() {
-		return amountPaid;
-	}
-	public void setAmountPaid(BigDecimal amountPaid) {
-		this.amountPaid = amountPaid;
-	}
-	public BigDecimal getOutstandingAmt() {
-		return outstandingAmt;
-	}
-	public void setOutstandingAmt(BigDecimal outstandingAmt) {
-		this.outstandingAmt = outstandingAmt;
-	}
+    public void setLoanId(String loanId) {
+        this.loanId = loanId;
+    }
+    
+    public LocalDate getDate() {
+        return date;
+    }
+    
+    public void setDate(LocalDate date) {
+        this.date = date;
+    }
+    
+    public String getType() {
+        return type;
+    }
+    
+    public void setType(String type) {
+        this.type = type;
+    }
+    
+    public BigDecimal getTotalLoan() {
+        return totalLoan;
+    }
+    
+    public void setTotalLoan(BigDecimal totalLoan) {
+        this.totalLoan = totalLoan;
+    }
+    
+    public BigDecimal getAmountPaid() {
+        return amountPaid;
+    }
+    
+    public void setAmountPaid(BigDecimal amountPaid) {
+        this.amountPaid = amountPaid;
+    }
+    
+    public BigDecimal getOutstandingAmt() {
+        return outstandingAmt;
+    }
+    
+    public void setOutstandingAmt(BigDecimal outstandingAmt) {
+        this.outstandingAmt = outstandingAmt;
+    }
 
-	public String getLoanId() {
-		return loanId;
-	}
-
-	public void setLoanId(String loanId) {
-		this.loanId = loanId;
-	}
-	@Override
-	public String toString() {
-		return "Loans [loanId=" + loanId + ", date=" + date + ", type=" + type + ", totalLoan=" + totalLoan + ", amountPaid=" + amountPaid
-			+ ", outstandingAmt=" + outstandingAmt + "]";
-	}
+    @Override
+    public String toString() {
+        return "Loans [loanId=" + loanId + ", date=" + date + ", type=" + type + ", totalLoan=" + totalLoan + ", amountPaid=" + amountPaid
+            + ", outstandingAmt=" + outstandingAmt + "]";
+    }
 }
